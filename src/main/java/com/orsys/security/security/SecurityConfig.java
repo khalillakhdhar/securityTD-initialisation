906b0f81970af250f392aeb1ba36f8b84fc2f68f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-10 13:56:38 +0100

    Update SecurityConfig.java

--- src/main/java/com/orsys/security/security/SecurityConfig.java
+++ src/main/java/com/orsys/security/security/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.orsys.security.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,5 +15,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 auth.inMemoryAuthentication().withUser("admin").password("{noop}1234").roles("USER","ADMIN");
 auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
+}
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+http.formLogin();
+http.csrf().disable();
+http.authorizeRequests().antMatchers("/login/**","register/**").permitAll();
+http.authorizeRequests().antMatchers(HttpMethod.POST,"/tasks/**").hasRole("ADMIN");
+http.authorizeRequests().anyRequest().authenticated();
 }
 }
